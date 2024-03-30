3201b3645d5b32fdd21d0b06d6a18eb9f26d79a2
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-03-30 00:43:42 +0000

    Data modeling refactoring

--- data_modeling.py
+++ data_modeling.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import requests
+from io import StringIO 
 from csv_data_dao import CVSDataDAO
 from api_resource_manager import ApiResourceManager
 
 
 #check data model with sample file
-df = pd.read_csv("sample.csv")
+#df = pd.read_csv("sample.csv")
 
 # Define API URL
 api_manager = ApiResourceManager()
-base_name = "YTTD"
+base_name = "GTTD"
 year = '2015'
 api_url = api_manager.api_resource(base_name+year)
 
@@ -24,34 +25,38 @@ except requests.exceptions.RequestException as e:
 
 # Convert data to DataFrame
 if data:
-    df = pd.read_csv(pd.compat.StringIO(data))
+    df = pd.read_csv(StringIO(data)) 
 
     # Convert from object format to data
-    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'])
+    df['lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'])
+
+    # Delete the column 'store_and_fwd_flag'
+    df = df.drop(columns=['store_and_fwd_flag'])
 
     df = df.drop_duplicates().reset_index(drop=True)
     df['trip_id'] = df.index
+  
+    datetime_dim = df[['lpep_pickup_datetime','lpep_dropoff_datetime']].reset_index(drop=True)
+    datetime_dim['lpep_pickup_datetime'] = datetime_dim['lpep_pickup_datetime']
+    datetime_dim['pick_hour'] = datetime_dim['lpep_pickup_datetime'].dt.hour
+    datetime_dim['pick_day'] = datetime_dim['lpep_pickup_datetime'].dt.day
+    datetime_dim['pick_month'] = datetime_dim['lpep_pickup_datetime'].dt.month
+    datetime_dim['pick_year'] = datetime_dim['lpep_pickup_datetime'].dt.year
+    datetime_dim['pick_weekday'] = datetime_dim['lpep_pickup_datetime'].dt.weekday
 
-    datetime_dim = df[['tpep_pickup_datetime','tpep_dropoff_datetime']].reset_index(drop=True)
-    datetime_dim['tpep_pickup_datetime'] = datetime_dim['tpep_pickup_datetime']
-    datetime_dim['pick_hour'] = datetime_dim['tpep_pickup_datetime'].dt.hour
-    datetime_dim['pick_day'] = datetime_dim['tpep_pickup_datetime'].dt.day
-    datetime_dim['pick_month'] = datetime_dim['tpep_pickup_datetime'].dt.month
-    datetime_dim['pick_year'] = datetime_dim['tpep_pickup_datetime'].dt.year
-    datetime_dim['pick_weekday'] = datetime_dim['tpep_pickup_datetime'].dt.weekday
-
-    datetime_dim['tpep_dropoff_datetime'] = datetime_dim['tpep_dropoff_datetime']
-    datetime_dim['drop_hour'] = datetime_dim['tpep_dropoff_datetime'].dt.hour
-    datetime_dim['drop_day'] = datetime_dim['tpep_dropoff_datetime'].dt.day
-    datetime_dim['drop_month'] = datetime_dim['tpep_dropoff_datetime'].dt.month
-    datetime_dim['drop_year'] = datetime_dim['tpep_dropoff_datetime'].dt.year
-    datetime_dim['drop_weekday'] = datetime_dim['tpep_dropoff_datetime'].dt.weekday
+    datetime_dim['lpep_dropoff_datetime'] = datetime_dim['lpep_dropoff_datetime']
+    datetime_dim['drop_hour'] = datetime_dim['lpep_dropoff_datetime'].dt.hour
+    datetime_dim['drop_day'] = datetime_dim['lpep_dropoff_datetime'].dt.day
+    datetime_dim['drop_month'] = datetime_dim['lpep_dropoff_datetime'].dt.month
+    datetime_dim['drop_year'] = datetime_dim['lpep_dropoff_datetime'].dt.year
+    datetime_dim['drop_weekday'] = datetime_dim['lpep_dropoff_datetime'].dt.weekday
 
     datetime_dim['datetime_id'] = datetime_dim.index
 
-    datetime_dim = datetime_dim[['datetime_id', 'tpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 'pick_year', 'pick_weekday',
-                                'tpep_dropoff_datetime', 'drop_hour', 'drop_day', 'drop_month', 'drop_year', 'drop_weekday']]
+    datetime_dim = datetime_dim[['datetime_id', 'lpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 
+                                'pick_year', 'pick_weekday','lpep_dropoff_datetime', 'drop_hour', 'drop_day', 
+                                'drop_month', 'drop_year', 'drop_weekday']]
 
     passenger_count_dim = df[['passenger_count']].reset_index(drop=True)
     passenger_count_dim['passenger_count_id'] = passenger_count_dim.index
@@ -60,7 +65,7 @@ if data:
     trip_distance_dim = df[['trip_distance']].reset_index(drop=True)
     trip_distance_dim['trip_distance_id'] = trip_distance_dim.index
     trip_distance_dim = trip_distance_dim[['trip_distance_id','trip_distance']]
-
+ 
     rate_code_type = {
         1:"Standard rate",
         2:"JFK",
@@ -69,20 +74,18 @@ if data:
         5:"Negotiated fare",
         6:"Group ride"
     }
-    rate_code_dim = df[['RatecodeID']].reset_index(drop=True)
+    rate_code_dim = df[['ratecodeid']].reset_index(drop=True)
     rate_code_dim['rate_code_id'] = rate_code_dim.index
-    rate_code_dim['rate_code_name'] = rate_code_dim['RatecodeID'].map(rate_code_type)
-    rate_code_dim = rate_code_dim[['rate_code_id','RatecodeID','rate_code_name']]
+    rate_code_dim['rate_code_name'] = rate_code_dim['ratecodeid'].map(rate_code_type)
+    rate_code_dim = rate_code_dim[['rate_code_id','ratecodeid','rate_code_name']]
 
-    pickup_location_dim = df[['PULocationID']].reset_index(drop=True)
+    pickup_location_dim = df[['pickup_longitude', 'pickup_latitude']].reset_index(drop=True)
     pickup_location_dim['pickup_location_id'] = pickup_location_dim.index
-    pickup_location_dim['pickup_location'] = pickup_location_dim['PULocationID']
-    pickup_location_dim = pickup_location_dim[['pickup_location_id','pickup_location']]
+    pickup_location_dim = pickup_location_dim[['pickup_location_id','pickup_latitude','pickup_longitude']] 
 
-    dropoff_location_dim = df[['DOLocationID']].reset_index(drop=True)
+    dropoff_location_dim = df[['dropoff_longitude', 'dropoff_latitude']].reset_index(drop=True)
     dropoff_location_dim['dropoff_location_id'] = dropoff_location_dim.index
-    dropoff_location_dim['dropoff_location'] = dropoff_location_dim['DOLocationID']
-    dropoff_location_dim = dropoff_location_dim[['dropoff_location_id', 'dropoff_location']]
+    dropoff_location_dim = dropoff_location_dim[['dropoff_location_id','dropoff_latitude','dropoff_longitude']]
 
     payment_type_name = {
         1:"Credit card",
@@ -96,7 +99,15 @@ if data:
     payment_type_dim['payment_type_id'] = payment_type_dim.index
     payment_type_dim['payment_type_name'] = payment_type_dim['payment_type'].map(payment_type_name)
     payment_type_dim = payment_type_dim[['payment_type_id','payment_type','payment_type_name']]
-
+  
+#    print(payment_type_dim.info())
+#    print(dropoff_location_dim.info())
+#    print(rate_code_dim.info())
+#    print(trip_distance_dim.info())
+#    print(passenger_count_dim.info())
+#    print(datetime_dim.info())
+#    print(df.info())
+          
     fact_table = df.merge(passenger_count_dim, left_on='trip_id', right_on='passenger_count_id') \
                 .merge(trip_distance_dim, left_on='trip_id', right_on='trip_distance_id') \
                 .merge(rate_code_dim, left_on='trip_id', right_on='rate_code_id') \
@@ -104,20 +115,23 @@ if data:
                 .merge(dropoff_location_dim, left_on='trip_id', right_on='dropoff_location_id')\
                 .merge(datetime_dim, left_on='trip_id', right_on='datetime_id') \
                 .merge(payment_type_dim, left_on='trip_id', right_on='payment_type_id') \
-                [['trip_id','VendorID', 'datetime_id', 'passenger_count_id',
-                'trip_distance_id', 'rate_code_id', 'store_and_fwd_flag', 'pickup_location_id', 'dropoff_location_id',
+                [['trip_id','vendorid', 'datetime_id', 'passenger_count_id',
+                'trip_distance_id', 'rate_code_id', 'pickup_location_id', 'dropoff_location_id',
                 'payment_type_id', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
                 'improvement_surcharge', 'total_amount']]
     
+#    print(fact_table.info())
+  
     # Create a DAO instance
     csv_dao = CVSDataDAO(api_url)
+    
     # Export DataFrame to SQL Server
     #TODO
-    server = 'your_server'
+    """server = 'your_server'
     database = 'your_database'
     table = 'your_table'
 
-    csv_dao.export_to_sql_server(df, server, database, table)
+    csv_dao.export_to_sql_server(df, server, database, table)"""
 else:
     print("No data fetched from API. Exiting...")
 
